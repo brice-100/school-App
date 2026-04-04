84d90fcb9bc693e1b5f022232ab6ef81f2913a3d
brice-10 <abbjm10@gmail.com>
2026-04-04 11:09:27 +0100

    fix: dotenv config

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors    = require('cors')
 const helmet  = require('helmet')
 const path    = require('path')
-require('dotenv').config()
+require('dotenv').config({ override: false })
 
 require('./config/db')
 
