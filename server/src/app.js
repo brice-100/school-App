02f26624d1758183022eabea05058df61f6a8590
brice-10 <abbjm10@gmail.com>
2026-04-06 15:36:05 +0100

    ajout des fichiers

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors    = require('cors')
 const helmet  = require('helmet')
 const path    = require('path')
-require('dotenv').config({ override: false })
+require('dotenv').config()
 
 require('./config/db')
 
